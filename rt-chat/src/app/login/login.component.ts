33e828d89805c2ead312624b20c503b25d5c7f73
yash1rj <yashrj130@gmail.com>
2019-09-07 22:09:26 +0530

    Update login.component.ts

--- rt-chat/src/app/login/login.component.ts
+++ rt-chat/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    this.loginForm=this.fb.group({
+      emailId: ['', Validators.required],
+      password: ['', Validators.required]
+    });
   }
 
 }
