b0c6f179ccaf60d3d2eb877b5dfc0f8ed92c4db0
yash1rj <yashrj130@gmail.com>
2019-09-07 22:09:11 +0530

    Update app-routing.module.ts

--- rt-chat/src/app/app-routing.module.ts
+++ rt-chat/src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
+import { RegisterationComponent } from './registeration/registeration.component';
+import { ChatComponent } from './chat/chat.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'home', component: LoginComponent },
+  { path: 'register', component: RegisterationComponent },
+  { path: 'chatroom', component: ChatComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
