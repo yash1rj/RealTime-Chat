d9759108f8a7686af350f10c2aae1c94a816ac52
yash1rj <yashrj130@gmail.com>
2019-09-08 00:51:57 +0530

    Update chat.component.ts

--- rt-chat/src/app/chat/chat.component.ts
+++ rt-chat/src/app/chat/chat.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../service/chat.service';
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+  constructor(private chatservice: ChatService) { }
+
+  chatArr = [];
 
   ngOnInit() {
+    this.chatservice.getchats().subscribe(res => {
+      console.log(res);
+      this.chatArr = res;
+      console.log(res[0].id);
+      console.log(this.chatArr);
+    }, err => {
+      console.log(err.message);
+    });
   }
-
 }
