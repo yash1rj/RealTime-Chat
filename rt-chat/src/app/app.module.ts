e97c498b6b2213870845a25b10af6bd2daa71d95
yash1rj <yashrj130@gmail.com>
2019-09-07 16:50:55 +0530

    Initial commit

---
+++ rt-chat/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterationComponent } from './registeration/registeration.component';
+import { ChatComponent } from './chat/chat.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterationComponent,
+    ChatComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
