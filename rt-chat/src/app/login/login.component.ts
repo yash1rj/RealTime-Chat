79cd29560f3445f5b5391b765b161c32593005af
yash1rj <yashrj130@gmail.com>
2019-09-09 00:45:54 +0530

    Update login.component.ts

--- rt-chat/src/app/login/login.component.ts
+++ rt-chat/src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { LoginService } from '../service/login.service';
+import { Validations } from '../validators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,14 +12,49 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
-  constructor(private fb: FormBuilder) { }
+  loginbody: any[];
+  allUsers: any[];
+  passworderrormsg: string = "Please enter 8-18 characters. Password should include at least 1 small letter, 1 capital letter and a number";
+  emailerrormsg: string = "Please enter valid email Id."
+  constructor(private fb: FormBuilder, private loginservice: LoginService, private loginvalidator: Validations, private router: Router) { }
 
   ngOnInit() {
 
-    this.loginForm=this.fb.group({
-      emailId: ['', Validators.required],
-      password: ['', Validators.required]
+    this.loginForm = this.fb.group({
+      emailId: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, this.loginvalidator.validateLogin]]
     });
+
+    this.loginservice.getUsers().subscribe(
+      (data) => {
+        // console.log(data);
+        this.allUsers = data;
+      },
+      (error) => {
+        console.log(error.message);
+      }
+    );
   }
 
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    this.loginbody = this.loginForm.value;
+    // console.log(this.loginbody);
+    for(let i = 0; i < this.allUsers.length; i++) {
+      if((this.loginbody.emailId === this.allUsers[i].emailId) && (this.loginbody.password === this.allUsers[i].password)) {
+        this.router.navigate(['chatroom']);
+      }
+      
+    }
+    // this.registerservice.regUser(this.regbody).subscribe(
+    //     (data)  => {
+    //       console.log("POST Request is successful ", data);
+    //       this.registerForm.reset();
+    //       this.router.navigate(['login']);
+    //     },
+    //     (error)  => {
+    //       console.log(error.message);
+    //     }
+    //   );
+  }
 }
