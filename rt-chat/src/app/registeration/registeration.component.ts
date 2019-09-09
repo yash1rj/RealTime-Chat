bf411d95fc21dd849da01e321e474eba8798b6e9
Yash Raj <yashrj130@gmail.com>
2019-09-09 18:29:20 +0530

    updating component

--- rt-chat/src/app/registeration/registeration.component.ts
+++ rt-chat/src/app/registeration/registeration.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { RegisterService } from '../service/register.service';
+import { Validations } from '../validators';
 import { Router } from '@angular/router';
 
 @Component({
@@ -13,15 +14,19 @@ export class RegisterationComponent implements OnInit {
   registerForm: FormGroup;
   regbody: object;
   islogged: boolean;
-  constructor(private fb: FormBuilder, private registerservice: RegisterService, private router: Router) { }
+  passworderrormsg: string = "Please enter 8-18 characters. Password should include at least 1 small letter, 1 capital letter and a number";
+  emailerrormsg: string = "Please enter valid email Id.";
+  usernameerrormsg: string = "Username required with atleast 3 characters";
+  nameerrormsg: string = "Name required with atleast 3 characters";
+  constructor(private fb: FormBuilder, private registerservice: RegisterService, private router: Router, private loginvalidator: Validations) { }
 
   ngOnInit() {
 
     this.registerForm=this.fb.group({
-      username: ['', Validators.required],
-      name: ['', Validators.required],
-      emailId: ['', Validators.required],
-      password: ['', Validators.required],
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      emailId: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, this.loginvalidator.validateLogin]],
       islogged: ['false']
     });
   }
