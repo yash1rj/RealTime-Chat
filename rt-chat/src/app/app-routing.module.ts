c1ae7a98c23fa4a6438193b9e4e06cf0808729d5
yash1rj <yashrj130@gmail.com>
2019-09-12 00:19:33 +0530

    Update app-routing.module.ts

--- rt-chat/src/app/app-routing.module.ts
+++ rt-chat/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterationComponent } from './registeration/registeration.component';
 import { ChatComponent } from './chat/chat.component';
+import { CanActivateRouteGuard } from './can-activate-route.guard';
 
 
 const routes: Routes = [
   { path: 'home', component: LoginComponent },
   { path: 'register', component: RegisterationComponent },
-  { path: 'chatroom', component: ChatComponent },
+  { path: 'chatroom', component: ChatComponent, canActivate: [CanActivateRouteGuard] },
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
 ];
 
