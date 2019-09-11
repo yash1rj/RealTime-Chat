fb3d321cdfc2da9fa7e403a9d4ab727222bc17dc
yash1rj <yashrj130@gmail.com>
2019-09-11 08:41:20 +0530

    Update chat.component.ts

--- rt-chat/src/app/chat/chat.component.ts
+++ rt-chat/src/app/chat/chat.component.ts
@@ -13,7 +13,9 @@ export class ChatComponent implements OnInit {
 
   chatArr = [];
   allUsers: any[];
+  allLoggedUsers: any[];
   loggedUser: string;
+  chatbody;
 
   ngOnInit() {
     this.chatservice.getchats().subscribe(res => {
@@ -28,19 +30,39 @@ export class ChatComponent implements OnInit {
     this.loginservice.getUsers().subscribe(
       (data) => {
         // console.log(data);
-        this.allUsers = data;
-        for (let i = 0; i < this.allUsers.length; i++) {
-          console.log(this.allUsers[i].islogged);
-          if (this.allUsers[i].islogged) {
-            this.loggedUser = this.allUsers[i].username;
-            console.log(this.loggedUser);
-            break;
-          }
-        }
+        this.loggedUser = sessionStorage.getItem("loggedUser");
+        this.allLoggedUsers = data.filter(function (user) {
+          return user.islogged == 'true';
+        });
+        // console.log(this.allLoggedUsers);
+        // console.log(this.loggedUser);
       },
       (error) => {
         console.log(error.message);
       }
     );
   }
+
+  onSubmit(val) {
+    // console.log(val);
+    this.chatbody = {
+      "username": this.loggedUser,
+      "msg": val
+    };
+    // console.log(this.chatbody);
+    this.chatservice.uploadchats(this.chatbody).subscribe(res => {
+      // console.log(res);
+      // this.chatArr = res;
+      // console.log(res[0].id);
+      // console.log(this.chatArr);
+    }, err => {
+      console.log(err.message);
+    });
+  }
+
+  sendMessageOnEnter($event, messagebox) {
+    if ($event.which === 13) { // ENTER_KEY
+        this.onSubmit(messagebox);
+    }
+}
 }
