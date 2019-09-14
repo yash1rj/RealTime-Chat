63b647390a8b9618aefa225715087d23828ef34e
yash1rj <yashrj130@gmail.com>
2019-09-14 16:11:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+
 const port = process.env.PORT || 3000;
 
-server.use(middlewares);
-server.use(router);
+app.use('/api', middlewares);
+app.use('/api', router);
 
-server.listen(port);
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/rtc-v1'));
+
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/rtc-v1/index.html'));
+});
+
+app.listen(port, () => console.log(`RT-Chat app listening on port ${port}!`))
