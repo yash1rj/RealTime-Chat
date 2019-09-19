900a8b2cb547d152666f1b8a131bece2d856e44a
yash1rj <yashrj130@gmail.com>
2019-09-19 22:58:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use('/api', middlewares);
 app.use('/api', router);
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/rtc-v1'));
+app.use(express.static(__dirname + '/dist/rt-chat'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/rtc-v1/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/rt-chat/index.html'));
 });
 
 app.listen(port, () => console.log(`RT-Chat app listening on port ${port}!`))
